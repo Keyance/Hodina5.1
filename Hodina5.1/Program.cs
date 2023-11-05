28d69b8bb479bf414cf4a818aa90b478e8f24876
Keyance <klara.konopacova@seznam.cz>
2023-11-05 11:10:57 +0100

    Přidejte soubory projektu.

---
+++ Hodina5.1/Program.cs
@@ -0,0 +1,54 @@
+﻿//Dictionary<string, List <string>> telefonniSeznam = new Dictionary<string, List<string>> (); ale dá se vytvořit kratší zápis
+var telefonniSeznam = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+bool jeKonec = false;
+
+while (!jeKonec)
+{
+    Console.WriteLine("1 - přidat osobu");
+    Console.WriteLine("2 - smazat osobu");
+    Console.WriteLine("3 - Vypsat osoby");
+    Console.WriteLine("0 - Konec");
+
+    int volba = Convert.ToInt32(Console.ReadLine());
+
+
+    switch (volba)
+    {
+        case 0:
+            jeKonec = true;
+            break;
+        case 1:
+            {
+                Console.WriteLine("Zadej jméno:");
+                var jmeno = Console.ReadLine();
+                if (!telefonniSeznam.ContainsKey(jmeno))
+                {
+                    Console.WriteLine("Zadej telefonní číslo:");
+                    string cislo = Console.ReadLine();
+                    telefonniSeznam.Add(jmeno, cislo);
+                }
+                else { Console.WriteLine("Toto jméno již v seznamu existuje."); }
+                break;
+            }
+        case 2:
+            //zde jsou složené závorky, aby byla proměnná uvnitř nich (jméno) pouze lokální, jinak tady být nemusí
+            {
+                Console.WriteLine("Zadej jméno:");
+                string jmeno = Console.ReadLine();
+                if (telefonniSeznam.Remove(jmeno))
+                {
+                    Console.WriteLine("Položka úspěšně smazána.");
+                } else
+                {
+                    Console.WriteLine("Položka neexistuje.");
+                }
+                break;
+            }
+        case 3:
+            foreach (KeyValuePair<string, string> kontakt in telefonniSeznam)
+            {
+                Console.WriteLine($"{kontakt.Key}\t{kontakt.Value}");
+            }
+            break;
+    }
+}
